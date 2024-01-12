fcc42c376c376d74e05a14c4898de612bc380e4e
André Steenhoff Madsen <andre@steenhoff.dk>
2024-01-12 15:46:10 +0100

    refactor: improves profile description

--- src/shared/localization/profile-localization.tsx
+++ src/shared/localization/profile-localization.tsx
@@ -25,7 +25,7 @@ const defaultProfileLocalization: ProfileLocalization = {
     componentLocalization: {
         sectionHeader: 'Profile',
         description: 
-        `Young logical thinking quick learning developer who is seeking a new job. Additionally i am also a problemsolver, who generally thinks innovatively.
+        `Young logical thinking quick learning developer. Additionally i am also a problem solver, who generally thinks innovatively.
         Lastly i am educated in the use of Scrum, and the use of its many UML models.`
     }
 }
@@ -43,7 +43,7 @@ export const danishProfileLocalization: ProfileLocalization = {
         ...defaultProfileLocalization.componentLocalization,
         sectionHeader: 'Profil',
         description: 
-        `Ung lærenem logisk tænkende udvikler der søger job. Er en rigtig god problemløser, som tænker innovativt. 
-        Er også oplært i brugen af Scrum, med dens dertil mange UML modeller.`
+        `Ung lærenem logisk tænkende udvikler. Er også en rigtig god problemløser, som tænker innovativt. 
+        Er oplært i brugen af Scrum, med dens dertil mange UML modeller.`
     }
 }
